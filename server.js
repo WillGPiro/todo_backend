9ad00d73550d7c691c4d7c776aa9a3bf5b789c92
Will G Piro <willgpiro@gmail.com>
2020-02-26 10:30:23 -0800

    run setup db and added first endpoint

---
+++ server.js
@@ -0,0 +1,110 @@
+// Load Environment Variables from the .env file
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const client = require('./client');
+// Initiate database connection
+client.connect();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // server files from /public folder
+app.use(express.json()); // enable reading incoming json data
+// API Routes
+
+app.use(express.urlencoded({ extended: true }));
+
+// *** TODOS ***
+//this get request returns a list of todos.
+app.get('/api/todos', async (req, res) => {
+
+    try {
+        const result = await client.query(`
+            select * from todos;
+        `);
+
+        res.json(result.rows);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+
+});
+
+app.post('/api/todos', async (req, res) => {
+    try {
+        
+        const query = `
+            insert into todos (task, complete)
+            values ('${req.body.task}', false)
+            returning *;
+        `;
+
+        const result = await client.query(`
+            insert into todos (task, complete)
+            values ('${body.req.task}',false)
+            returning *;  
+        `,
+
+        [req.params.todos]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.put('/api/todos/:id', async (req, res) => {
+    const id = req.params.id;
+    const todo = req.body;
+
+    try {
+        const result = await client.query(`
+            
+        `, [/* pass in data */]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.delete('/api/todos/:id', async (req, res) => {
+    // get the id that was passed in the route:
+
+    try {
+        const result = await client.query(`
+
+        `, [/* pass data */]);
+
+        res.json(result.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('server running on PORT', PORT);
+});
